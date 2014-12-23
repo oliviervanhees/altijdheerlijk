223c806de0d4a923a9b7dd44de4cda86896ef948
Olivier van Hees <oliviervanhees@gmail.com>
2014-12-24 00:35:45 +0100

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
       where(provider2: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
+        user.provider2 = auth.provider
         user.uid2 = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
