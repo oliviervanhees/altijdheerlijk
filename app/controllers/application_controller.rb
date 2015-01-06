290e57e322d987524639c347d73d69a42ac5e6b3
Olivier van Hees <oliviervanhees@gmail.com>
2015-01-06 23:48:20 +0100

    changed application controller for profile images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+before_action :authenticate_user!
 
- before_filter :configure_permitted_parameters, if: :devise_controller?
+protect_from_forgery with: :exception
+before_filter :configure_permitted_parameters, if: :devise_controller?
 
 protected
 
- def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :image
-   devise_parameter_sanitizer.for(:account_update) << :image
- end
-
- def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :name
-   devise_parameter_sanitizer.for(:account_update) << :name
- end
+def configure_permitted_parameters
+   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
+   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
+end
 
 end
