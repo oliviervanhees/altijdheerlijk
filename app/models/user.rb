48985bfbce9c1ec9990fd84b60893831de2fa193
Olivier van Hees <oliviervanhees@gmail.com>
2015-01-07 00:50:52 +0100

    changed some frontend stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,6 @@ class User < ActiveRecord::Base
   end
 
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :square => "200x200>", :thumb => "100x100>" }
   
 end
