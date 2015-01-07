b5ce220900984f320dc15dcd8db27996df384512
Olivier van Hees <oliviervanhees@gmail.com>
2015-01-07 13:20:38 +0100

    changed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ protected
 
 def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
+   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
 end
 
