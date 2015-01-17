1839762a9cd564ceeddda3278386ad706477e40c
Olivier van Hees <oliviervanhees@gmail.com>
2015-01-17 18:41:05 +0100

    added default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,7 @@ class User < ActiveRecord::Base
       end
   end
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default.jpg"
+  has_attached_file :image_file_name, 
+  :styles => { :medium => '60x60#', :thumb => '300x300#' }, :default_style => :thumb,
+  :default_url => "/assets/:style/default.jpg"
 end
