99289dafc7789659765fdc704c8a42f06166fa42
Olivier van Hees <oliviervanhees@gmail.com>
2015-01-28 16:54:53 +0100

    facebook foto added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ before_filter :configure_permitted_parameters, if: :devise_controller?
 protected
 
 def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
-   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
-   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
+   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :image_file_name, :email, :password, :password_confirmation, :current_password) }
+   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :image_file_name, :email, :password, :password_confirmation, :current_password) }
+   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :image_file_name, :email, :password, :password_confirmation, :current_password) }
 end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
         user.uid2 = auth.uid
         user.email = auth.info.email
         user.name = auth.info.name
+        user.image_file_name = auth.info.image
         user.password = Devise.friendly_token[0,20]
       end
   end
