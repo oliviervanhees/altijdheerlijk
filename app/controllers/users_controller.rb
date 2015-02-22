a71a8185a56b24e5c387b1513c6f8cc3c4559ba8
Olivier van Hees <oliviervanhees@gmail.com>
2015-02-22 11:35:46 +0100

    solved bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
 def show
 	@user = User.find(params[:id])
-
 	respond_to do |format|
 	  format.html # show.html.erb
 	  format.xml  { render :xml => @user }
@@ -26,7 +25,5 @@ def unlike
 	@user.unliked_by current_user
 	redirect_to :back
 end
-
-
 
   end
